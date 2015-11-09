b3a18c650cd3b51398e63f7ad503ffa64786c56b
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-09 21:28:45 +0100

    computed-properties

--- app/user/model.js
+++ app/user/model.js
@@ -4,19 +4,27 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   username: DS.attr('string'),
-  numberOfChirps: DS.attr('number'),
-  numberOfFollowing: DS.attr('number'),
-  numberOfFollowers: DS.attr('number'),
-
   aboutMe: DS.attr('string'),
   joinedAt: DS.attr('date'),
+
   chirps: DS.hasMany('chirp', { async: true }),
+  numberOfChirps: Ember.computed('chirps', function() {
+    return this.get('chirps').get('length');
+  }),
 
   followees: DS.hasMany('user', {
   	inverse: 'followers'
   }),
   followers: DS.hasMany('user', {
   	inverse: 'followers'
+  }),
+
+  numberOfFollowing: Ember.computed('followees', function() {
+    return this.get('followees').get('length');
+  }),
+  
+  numberOfFollowers: Ember.computed('followers', function() {
+    return this.get('followers').get('length');
   })
 
 });
