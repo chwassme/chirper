ed9ba913716986c10a37cda2031a89b096e67375
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-09 21:38:19 +0100

    add to build-process

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,9 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/sweetalert/dist/sweetalert.min.js');
+  app.import('bower_components/sweetalert/dist/sweetalert.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
