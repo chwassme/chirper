1b94047bc7b552b13d5c435962c8872996d05a6d
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-11 09:48:17 +0100

    cursor in text-area when dialog opens

--- app/components/compose-modal/component.js
+++ app/components/compose-modal/component.js
@@ -1,4 +1,13 @@
+// app/components/compose-modal/component.js
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  
+  focusOnTextarea: Ember.on('didInsertElement', function() {
+    Ember.run.scheduleOnce('afterRender', () => {
+      this.$().find('textarea').focus();
+    });
+  })
+  
 });
