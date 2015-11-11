e761dec97296af08bfbea24aa7e4f6222065bbbf
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-11 11:17:05 +0100

    support for signup and login

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,19 +1,35 @@
 // app/index/controller.js
 
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Controller.extend({
   actions: {
-    login: function() {
+    signup: function() {
+    var userData = {
+      username: this.get('username'),
+      password: this.get('password')
+    };
+
+    Ember.$.ajax({
+      type: 'POST',
+      url: config.apiURL + '/signup',
+      dataType: 'json',
+      data: userData
+    })
+    .done(() => {
+      console.log("Created!");
+      this.send('login');
+    })
+    .fail(() => {
+      this.set('errorMessage', "Couldn't sign up!");
+    });
+  },
+
+  login: function() {
       var username = this.get('username');
       var password = this.get('password');
-
-      if (username === 'letme' && password === 'in') {
-		this.transitionToRoute('home');
-	  } else {
-		// We replace our SweetAlert-function with a simple variable containing the error message
-		this.set('errorMessage', 'Wrong username or password!');
-	  }
+      console.log(`Trying to log in with ${username} and ${password}`);  
     }
   },
   valuesChanged: Ember.observer('username', 'password', function() {
