9897693888320b7277f9d6a80ec25ab64702841c
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-11 09:42:55 +0100

    open modal-dialog on status-update

---
+++ app/home/controller.js
@@ -0,0 +1,13 @@
+// app/home/controller.js
+
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  application: Ember.inject.controller(), // This lets us access the application controller's actions
+  
+  actions: {
+    openComposeModal: function() {
+      this.get('application').send('toggleComposeModal');
+    }
+  }
+});
