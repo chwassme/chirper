589ed678c443a5c2dee35475002f7e0e562d5c90
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-11 08:59:48 +0100

    fake login

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,4 +1,18 @@
+// app/index/controller.js
+
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    login: function() {
+      var username = this.get('username');
+      var password = this.get('password');
+
+      if (username === 'letme' && password === 'in') {
+        this.transitionToRoute('home');
+      } else {
+        sweetAlert('Oops!', 'Wrong username or password!', 'error');
+      }
+    }
+  }
 });
