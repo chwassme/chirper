72dfc9cf1ce7c2b229bc8c2f225273fb289d5e08
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-11 09:58:45 +0100

    postChirp

--- app/components/compose-modal/component.js
+++ app/components/compose-modal/component.js
@@ -10,6 +10,34 @@ export default Ember.Component.extend({
     });
   }),
 
-  store: Ember.inject.service()
+  store: Ember.inject.service(),
+
+  actions: {
+   postChirp: function() {
+    // Retrieve the value of {{textarea}}
+    var text = this.get('chirpText');
+
+    // Fetch the user before adding it to the record
+    // (Remember that our chirp model uses DS.belongsTo('user'))
+    this.get('store').findRecord('user', 1).then((user) => {
+
+      // Set the data that you want
+      var chirpData = {
+        text: text,
+        user: user,
+        createdAt: new Date()
+      };
+
+      // Create the record
+      var newChirp = this.get('store').createRecord('chirp', chirpData);
+
+      // Save the record so that it shows up in the DOM
+      return newChirp.save();
+
+    }).then(() => {
+      this.attrs.dismiss(); // Bubble up the action
+    });
+  }
+}
   
 });
