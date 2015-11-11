449a1d5f1f4eca9b726663f29d5295b344edaf18
Christian Wassmer <chwassme@hockeyfans.ch>
2015-11-11 13:53:43 +0100

    follower stuff - stopping tutorial here - sth broke :-(

--- app/components/follow-button/component.js
+++ app/components/follow-button/component.js
@@ -1,4 +1,12 @@
+// app/components/follow-button/component.js
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['follow-container'],
+
+  isFollowing: Ember.computed('profile.followers', 'session.currentUser', function() {
+    return this.get('profile.followers').isAny('id', this.get('session.currentUser.id'));
+  }),
+  
 });
